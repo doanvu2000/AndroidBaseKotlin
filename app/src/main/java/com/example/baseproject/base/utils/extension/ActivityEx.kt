030adf65c4e0eab62c635cfb901e61e61365e185
Doan Vũ <doanvu20122000@gmail.com>
2023-09-20 09:55:55 +0700

    get screen width, height

--- app/src/main/java/com/example/baseproject/base/utils/extension/ActivityEx.kt
+++ app/src/main/java/com/example/baseproject/base/utils/extension/ActivityEx.kt
@@ -10,6 +10,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.provider.Settings
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.WindowInsets
@@ -192,5 +193,31 @@ fun Activity.registerBroadcast(
         registerReceiver(broadcastReceiver, intentFilter, receiverFlags)
     } else {
         registerReceiver(broadcastReceiver, intentFilter)
+    }
+}
+
+fun Activity.getScreenWidth(): Int {
+    return if (isSdkR()) {
+        val windowMetrics = windowManager.currentWindowMetrics
+        val insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+        windowMetrics.bounds.width() - insets.left - insets.right
+    } else {
+        val displayMetrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayMetrics.widthPixels
+    }
+}
+
+fun Activity.getScreenHeight(): Int {
+    return if (isSdkR()) {
+        val windowMetrics = windowManager.currentWindowMetrics
+        val insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+        windowMetrics.bounds.height() - insets.top - insets.bottom
+    } else {
+        val displayMetrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayMetrics.heightPixels
     }
 }
