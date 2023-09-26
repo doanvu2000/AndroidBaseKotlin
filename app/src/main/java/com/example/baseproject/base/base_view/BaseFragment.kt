26d4cc4f32c91dbb6b99105b8c94da54c24e3f54
Doan Vũ <doanvu20122000@gmail.com>
2023-09-26 16:09:46 +0700

    - update BaseFragment.kt

--- app/src/main/java/com/example/baseproject/base/base_view/BaseFragment.kt
+++ app/src/main/java/com/example/baseproject/base/base_view/BaseFragment.kt
@@ -9,10 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
-
-    lateinit var binding: VB
+    private var _binding: VB? = null
+    private val binding: VB get() = _binding!!
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = inflateLayout(inflater, container)
+        _binding = inflateLayout(inflater, container)
         return binding.root
     }
 
@@ -35,6 +35,11 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
     open fun onBack() {
         requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     abstract fun initView()
