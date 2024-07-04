cbe45582318f3d5df23040912ea880303f5d6ccd
doanvu20122000 <doanvu20122000@gmail.com>
2024-07-04 09:34:20 +0700

    add function launch with coroutine in fragment

--- app/src/main/java/com/example/baseproject/base/base_view/BaseFragment.kt
+++ app/src/main/java/com/example/baseproject/base/base_view/BaseFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.example.baseproject.base.utils.extension.handleBackPressed
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     private var _binding: VB? = null
@@ -45,4 +50,44 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
     /**override it and inflate your view binding, demo in HomeFragment*/
     abstract fun inflateLayout(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    fun launchAction(dispatcher: CoroutineContext, action: () -> Unit) {
+        view?.let {
+            viewLifecycleOwner.lifecycleScope.launch(dispatcher) {
+                action()
+            }
+        }
+    }
+
+    private fun launchCoroutine(
+        dispatcher: CoroutineContext, blockCoroutine: suspend CoroutineScope.() -> Unit
+    ) {
+        try {
+            view?.let {
+                viewLifecycleOwner.lifecycleScope.launch(dispatcher) {
+                    blockCoroutine()
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun launchCoroutineMain(blockCoroutine: suspend CoroutineScope.() -> Unit) {
+        launchCoroutine(Dispatchers.Main) {
+            blockCoroutine()
+        }
+    }
+
+    fun launchCoroutineIO(blockCoroutine: suspend CoroutineScope.() -> Unit) {
+        launchCoroutine(Dispatchers.IO) {
+            blockCoroutine()
+        }
+    }
+
+    fun launchCoroutineDefault(blockCoroutine: suspend CoroutineScope.() -> Unit) {
+        launchCoroutine(Dispatchers.Default) {
+            blockCoroutine()
+        }
+    }
 }
