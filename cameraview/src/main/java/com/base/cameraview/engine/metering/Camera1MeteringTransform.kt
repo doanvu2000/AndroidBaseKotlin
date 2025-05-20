3993ce7f94c2a91d313e8f80e5257066fcac5276
doanvu20122000 <doanvu20122000@gmail.com>
2025-05-20 14:39:03 +0700

    Module cameraview: Move all class in com.base.cameraview.engine.metering from java to kotlin

--- cameraview/src/main/java/com/base/cameraview/engine/metering/Camera1MeteringTransform.kt
+++ cameraview/src/main/java/com/base/cameraview/engine/metering/Camera1MeteringTransform.kt
@@ -1,56 +1,47 @@
-package com.base.cameraview.engine.metering;
+package com.base.cameraview.engine.metering
 
-import android.graphics.PointF;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.hardware.Camera;
+import android.graphics.PointF
+import android.graphics.Rect
+import android.graphics.RectF
+import android.hardware.Camera
+import com.base.cameraview.CameraLogger
+import com.base.cameraview.engine.offset.Angles
+import com.base.cameraview.engine.offset.Axis
+import com.base.cameraview.engine.offset.Reference
+import com.base.cameraview.metering.MeteringTransform
+import com.base.cameraview.size.Size
+import kotlin.math.cos
+import kotlin.math.sin
 
-import androidx.annotation.NonNull;
+class Camera1MeteringTransform(angles: Angles, private val previewSize: Size) :
+    MeteringTransform<Camera.Area?> {
+    private val displayToSensor: Int =
+        -angles.offset(Reference.SENSOR, Reference.VIEW, Axis.ABSOLUTE)
 
-import com.base.cameraview.CameraLogger;
-import com.base.cameraview.engine.offset.Angles;
-import com.base.cameraview.engine.offset.Axis;
-import com.base.cameraview.engine.offset.Reference;
-import com.base.cameraview.metering.MeteringTransform;
-import com.base.cameraview.size.Size;
-
-
-public class Camera1MeteringTransform implements MeteringTransform<Camera.Area> {
-
-    protected static final String TAG = Camera1MeteringTransform.class.getSimpleName();
-    protected static final CameraLogger LOG = CameraLogger.create(TAG);
-
-    private final int displayToSensor;
-    private final Size previewSize;
-
-    public Camera1MeteringTransform(@NonNull Angles angles, @NonNull Size previewSize) {
-        this.displayToSensor = -angles.offset(Reference.SENSOR, Reference.VIEW, Axis.ABSOLUTE);
-        this.previewSize = previewSize;
-    }
-
-    @NonNull
-    @Override
-    public PointF transformMeteringPoint(@NonNull PointF point) {
+    override fun transformMeteringPoint(point: PointF): PointF {
         // First, rescale to the -1000 ... 1000 range.
-        PointF scaled = new PointF();
-        scaled.x = -1000F + (point.x / previewSize.getWidth()) * 2000F;
-        scaled.y = -1000F + (point.y / previewSize.getHeight()) * 2000F;
+        val scaled = PointF()
+        scaled.x = -1000f + (point.x / previewSize.width) * 2000f
+        scaled.y = -1000f + (point.y / previewSize.height) * 2000f
 
         // Apply rotation to this point.
         // https://academo.org/demos/rotation-about-point/
-        PointF rotated = new PointF();
-        double theta = ((double) displayToSensor) * Math.PI / 180;
-        rotated.x = (float) (scaled.x * Math.cos(theta) - scaled.y * Math.sin(theta));
-        rotated.y = (float) (scaled.x * Math.sin(theta) + scaled.y * Math.cos(theta));
-        LOG.i("scaled:", scaled, "rotated:", rotated);
-        return rotated;
+        val rotated = PointF()
+        val theta = (displayToSensor.toDouble()) * Math.PI / 180
+        rotated.x = (scaled.x * cos(theta) - scaled.y * sin(theta)).toFloat()
+        rotated.y = (scaled.x * sin(theta) + scaled.y * cos(theta)).toFloat()
+        LOG.i("scaled:", scaled, "rotated:", rotated)
+        return rotated
     }
 
-    @NonNull
-    @Override
-    public Camera.Area transformMeteringRegion(@NonNull RectF region, int weight) {
-        Rect rect = new Rect();
-        region.round(rect);
-        return new Camera.Area(rect, weight);
+    override fun transformMeteringRegion(region: RectF, weight: Int): Camera.Area {
+        val rect = Rect()
+        region.round(rect)
+        return Camera.Area(rect, weight)
+    }
+
+    companion object {
+        private val TAG: String = Camera1MeteringTransform::class.java.simpleName
+        private val LOG: CameraLogger = CameraLogger.create(TAG)
     }
 }
--- cameraview/src/main/java/com/base/cameraview/engine/metering/Camera2MeteringTransform.kt
+++ cameraview/src/main/java/com/base/cameraview/engine/metering/Camera2MeteringTransform.kt
@@ -1,194 +1,187 @@
-package com.base.cameraview.engine.metering;
+package com.base.cameraview.engine.metering
 
-import android.graphics.PointF;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.params.MeteringRectangle;
+import android.graphics.PointF
+import android.graphics.Rect
+import android.graphics.RectF
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.params.MeteringRectangle
+import com.base.cameraview.CameraLogger
+import com.base.cameraview.engine.offset.Angles
+import com.base.cameraview.engine.offset.Axis
+import com.base.cameraview.engine.offset.Reference
+import com.base.cameraview.metering.MeteringTransform
+import com.base.cameraview.size.AspectRatio
+import com.base.cameraview.size.Size
+import kotlin.math.roundToInt
 
-import androidx.annotation.NonNull;
-
-import com.base.cameraview.CameraLogger;
-import com.base.cameraview.engine.offset.Angles;
-import com.base.cameraview.engine.offset.Axis;
-import com.base.cameraview.engine.offset.Reference;
-import com.base.cameraview.metering.MeteringTransform;
-import com.base.cameraview.size.AspectRatio;
-import com.base.cameraview.size.Size;
-
-public class Camera2MeteringTransform implements MeteringTransform<MeteringRectangle> {
-
-    protected static final String TAG = Camera2MeteringTransform.class.getSimpleName();
-    protected static final CameraLogger LOG = CameraLogger.create(TAG);
-
-    private final Angles angles;
-    private final Size previewSize;
-    private final Size previewStreamSize;
-    private final boolean previewIsCropping;
-    private final CameraCharacteristics characteristics;
-    private final CaptureRequest.Builder builder;
-
-    public Camera2MeteringTransform(@NonNull Angles angles,
-                                    @NonNull Size previewSize,
-                                    @NonNull Size previewStreamSize,
-                                    boolean previewIsCropping,
-                                    @NonNull CameraCharacteristics characteristics,
-                                    @NonNull CaptureRequest.Builder builder) {
-        this.angles = angles;
-        this.previewSize = previewSize;
-        this.previewStreamSize = previewStreamSize;
-        this.previewIsCropping = previewIsCropping;
-        this.characteristics = characteristics;
-        this.builder = builder;
+class Camera2MeteringTransform(
+    private val angles: Angles,
+    private val previewSize: Size,
+    private val previewStreamSize: Size,
+    private val previewIsCropping: Boolean,
+    private val characteristics: CameraCharacteristics,
+    private val builder: CaptureRequest.Builder
+) : MeteringTransform<MeteringRectangle?> {
+    override fun transformMeteringRegion(region: RectF, weight: Int): MeteringRectangle {
+        val round = Rect()
+        region.round(round)
+        return MeteringRectangle(round, weight)
     }
 
-    @NonNull
-    @Override
-    public MeteringRectangle transformMeteringRegion(@NonNull RectF region, int weight) {
-        Rect round = new Rect();
-        region.round(round);
-        return new MeteringRectangle(round, weight);
-    }
-
-    @NonNull
-    @Override
-    public PointF transformMeteringPoint(@NonNull PointF point) {
+    override fun transformMeteringPoint(point: PointF): PointF {
         // This is a good Q/A. https://stackoverflow.com/a/33181620/4288782
         // At first, the point is relative to the View system and does not account
         // our own cropping. Will keep updating these two below.
-        final PointF referencePoint = new PointF(point.x, point.y);
-        Size referenceSize = previewSize;
+        val referencePoint = PointF(point.x, point.y)
+        var referenceSize = previewSize
 
         // 1. Account for cropping.
         // This will enlarge the preview size so that aspect ratio matches.
-        referenceSize = applyPreviewCropping(referenceSize, referencePoint);
+        referenceSize = applyPreviewCropping(referenceSize, referencePoint)
 
         // 2. Scale to the preview stream coordinates.
         // This will move to the preview stream coordinates by scaling.
-        referenceSize = applyPreviewScale(referenceSize, referencePoint);
+        referenceSize = applyPreviewScale(referenceSize, referencePoint)
 
         // 3. Rotate to the stream coordinate system.
         // This leaves us with sensor stream coordinates.
-        referenceSize = applyPreviewToSensorRotation(referenceSize, referencePoint);
+        referenceSize = applyPreviewToSensorRotation(referenceSize, referencePoint)
 
         // 4. Move to the crop region coordinate system.
         // The crop region is the union of all currently active streams.
-        referenceSize = applyCropRegionCoordinates(referenceSize, referencePoint);
+        referenceSize = applyCropRegionCoordinates(referenceSize, referencePoint)
 
         // 5. Move to the active array coordinate system.
-        referenceSize = applyActiveArrayCoordinates(referenceSize, referencePoint);
-        LOG.i("input:", point, "output (before clipping):", referencePoint);
+        referenceSize = applyActiveArrayCoordinates(referenceSize, referencePoint)
+        LOG.i("input:", point, "output (before clipping):", referencePoint)
 
         // 6. Probably not needed, but make sure we clip.
-        if (referencePoint.x < 0) referencePoint.x = 0;
-        if (referencePoint.y < 0) referencePoint.y = 0;
-        if (referencePoint.x > referenceSize.getWidth())
-            referencePoint.x = referenceSize.getWidth();
-        if (referencePoint.y > referenceSize.getHeight())
-            referencePoint.y = referenceSize.getHeight();
-        LOG.i("input:", point, "output (after clipping):", referencePoint);
-        return referencePoint;
+        if (referencePoint.x < 0) referencePoint.x = 0f
+        if (referencePoint.y < 0) referencePoint.y = 0f
+        if (referencePoint.x > referenceSize.width) referencePoint.x =
+            referenceSize.width.toFloat()
+        if (referencePoint.y > referenceSize.height) referencePoint.y =
+            referenceSize.height.toFloat()
+        LOG.i("input:", point, "output (after clipping):", referencePoint)
+        return referencePoint
     }
 
 
-    @SuppressWarnings("UnnecessaryLocalVariable")
-    @NonNull
-    private Size applyPreviewCropping(@NonNull Size referenceSize, @NonNull PointF referencePoint) {
-        Size previewStreamSize = this.previewStreamSize;
-        Size previewSurfaceSize = referenceSize;
-        int referenceWidth = previewSurfaceSize.getWidth();
-        int referenceHeight = previewSurfaceSize.getHeight();
-        AspectRatio previewStreamAspectRatio = AspectRatio.of(previewStreamSize);
-        AspectRatio previewSurfaceAspectRatio = AspectRatio.of(previewSurfaceSize);
+    private fun applyPreviewCropping(referenceSize: Size, referencePoint: PointF): Size {
+        val previewStreamSize = this.previewStreamSize
+        val previewSurfaceSize = referenceSize
+        var referenceWidth = previewSurfaceSize.width
+        var referenceHeight = previewSurfaceSize.height
+        val previewStreamAspectRatio = AspectRatio.of(previewStreamSize)
+        val previewSurfaceAspectRatio = AspectRatio.of(previewSurfaceSize)
         if (previewIsCropping) {
             if (previewStreamAspectRatio.toFloat() > previewSurfaceAspectRatio.toFloat()) {
                 // Stream is larger. The x coordinate must be increased: a touch on the left side
                 // of the surface is not on the left size of stream (it's more to the right).
-                float scale = previewStreamAspectRatio.toFloat()
-                        / previewSurfaceAspectRatio.toFloat();
-                referencePoint.x += previewSurfaceSize.getWidth() * (scale - 1F) / 2F;
-                referenceWidth = Math.round(previewSurfaceSize.getWidth() * scale);
+                val scale = (previewStreamAspectRatio.toFloat()
+                        / previewSurfaceAspectRatio.toFloat())
+                referencePoint.x += previewSurfaceSize.width * (scale - 1f) / 2f
+                referenceWidth = (previewSurfaceSize.width * scale).roundToInt()
             } else {
                 // Stream is taller. The y coordinate must be increased: a touch on the top side
                 // of the surface is not on the top size of stream (it's a bit lower).
-                float scale = previewSurfaceAspectRatio.toFloat()
-                        / previewStreamAspectRatio.toFloat();
-                referencePoint.y += previewSurfaceSize.getHeight() * (scale - 1F) / 2F;
-                referenceHeight = Math.round(previewSurfaceSize.getHeight() * scale);
+                val scale = (previewSurfaceAspectRatio.toFloat()
+                        / previewStreamAspectRatio.toFloat())
+                referencePoint.y += previewSurfaceSize.height * (scale - 1f) / 2f
+                referenceHeight = (previewSurfaceSize.height * scale).roundToInt()
             }
         }
-        return new Size(referenceWidth, referenceHeight);
+        return Size(referenceWidth, referenceHeight)
     }
 
-    @NonNull
-    private Size applyPreviewScale(@NonNull Size referenceSize, @NonNull PointF referencePoint) {
+    private fun applyPreviewScale(referenceSize: Size, referencePoint: PointF): Size {
         // The referenceSize how has the same aspect ratio of the previewStreamSize, but they
         // can still have different size (that is, a scale operation is needed).
-        Size previewStreamSize = this.previewStreamSize;
-        referencePoint.x *= (float) previewStreamSize.getWidth() / referenceSize.getWidth();
-        referencePoint.y *= (float) previewStreamSize.getHeight() / referenceSize.getHeight();
-        return previewStreamSize;
+        val previewStreamSize = this.previewStreamSize
+        referencePoint.x *= previewStreamSize.width.toFloat() / referenceSize.width
+        referencePoint.y *= previewStreamSize.height.toFloat() / referenceSize.height
+        return previewStreamSize
     }
 
-    @SuppressWarnings("SuspiciousNameCombination")
-    @NonNull
-    private Size applyPreviewToSensorRotation(@NonNull Size referenceSize,
-                                              @NonNull PointF referencePoint) {
+    private fun applyPreviewToSensorRotation(
+        referenceSize: Size,
+        referencePoint: PointF
+    ): Size {
         // Not elegant, but the sin/cos way was failing for some reason.
-        int angle = angles.offset(Reference.SENSOR, Reference.VIEW, Axis.ABSOLUTE);
-        boolean flip = angle % 180 != 0;
-        float tempX = referencePoint.x;
-        float tempY = referencePoint.y;
-        if (angle == 0) {
-            referencePoint.x = tempX;
-            referencePoint.y = tempY;
-        } else if (angle == 90) {
-            referencePoint.x = tempY;
-            referencePoint.y = referenceSize.getWidth() - tempX;
-        } else if (angle == 180) {
-            referencePoint.x = referenceSize.getWidth() - tempX;
-            referencePoint.y = referenceSize.getHeight() - tempY;
-        } else if (angle == 270) {
-            referencePoint.x = referenceSize.getHeight() - tempY;
-            referencePoint.y = tempX;
-        } else {
-            throw new IllegalStateException("Unexpected angle " + angle);
+        val angle = angles.offset(Reference.SENSOR, Reference.VIEW, Axis.ABSOLUTE)
+        val flip = angle % 180 != 0
+        val tempX = referencePoint.x
+        val tempY = referencePoint.y
+        when (angle) {
+            0 -> {
+                referencePoint.x = tempX
+                referencePoint.y = tempY
+            }
+
+            90 -> {
+                referencePoint.x = tempY
+                referencePoint.y = referenceSize.width - tempX
+            }
+
+            180 -> {
+                referencePoint.x = referenceSize.width - tempX
+                referencePoint.y = referenceSize.height - tempY
+            }
+
+            270 -> {
+                referencePoint.x = referenceSize.height - tempY
+                referencePoint.y = tempX
+            }
+
+            else -> {
+                throw IllegalStateException("Unexpected angle $angle")
+            }
         }
-        return flip ? referenceSize.flip() : referenceSize;
+        return if (flip) referenceSize.flip() else referenceSize
     }
 
-    @NonNull
-    private Size applyCropRegionCoordinates(@NonNull Size referenceSize,
-                                            @NonNull PointF referencePoint) {
+    private fun applyCropRegionCoordinates(
+        referenceSize: Size,
+        referencePoint: PointF
+    ): Size {
         // The input point and size refer to the stream rect.
         // The stream rect is part of the 'crop region', as described below.
         // https://source.android.com/devices/camera/camera3_crop_reprocess.html
-        Rect cropRect = builder.get(CaptureRequest.SCALER_CROP_REGION);
+        val cropRect = builder.get(CaptureRequest.SCALER_CROP_REGION)
         // For now we don't care about x and y position. Rect should not be null, but let's be safe.
-        int cropRectWidth = cropRect == null ? referenceSize.getWidth() : cropRect.width();
-        int cropRectHeight = cropRect == null ? referenceSize.getHeight() : cropRect.height();
+        val cropRectWidth = cropRect?.width() ?: referenceSize.width
+        val cropRectHeight = cropRect?.height() ?: referenceSize.height
         // The stream is always centered inside the crop region, and one of the dimensions
         // should always match. We just increase the other one.
-        referencePoint.x += (cropRectWidth - referenceSize.getWidth()) / 2F;
-        referencePoint.y += (cropRectHeight - referenceSize.getHeight()) / 2F;
-        return new Size(cropRectWidth, cropRectHeight);
+        referencePoint.x += (cropRectWidth - referenceSize.width) / 2f
+        referencePoint.y += (cropRectHeight - referenceSize.height) / 2f
+        return Size(cropRectWidth, cropRectHeight)
     }
 
-    @NonNull
-    private Size applyActiveArrayCoordinates(@NonNull Size referenceSize,
-                                             @NonNull PointF referencePoint) {
+    private fun applyActiveArrayCoordinates(
+        referenceSize: Size,
+        referencePoint: PointF
+    ): Size {
         // The input point and size refer to the scaler crop region.
         // We can query for the crop region position inside the active array, so this is easy.
-        Rect cropRect = builder.get(CaptureRequest.SCALER_CROP_REGION);
-        referencePoint.x += cropRect == null ? 0 : cropRect.left;
-        referencePoint.y += cropRect == null ? 0 : cropRect.top;
+        val cropRect = builder.get(CaptureRequest.SCALER_CROP_REGION)
+        referencePoint.x += (cropRect?.left ?: 0).toFloat()
+        referencePoint.y += (cropRect?.top ?: 0).toFloat()
         // Finally, get the active rect width and height from characteristics.
-        Rect activeRect = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE);
+        var activeRect =
+            characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE)
         if (activeRect == null) { // Should never happen
-            activeRect = new Rect(0, 0, referenceSize.getWidth(),
-                    referenceSize.getHeight());
+            activeRect = Rect(
+                0, 0, referenceSize.width,
+                referenceSize.height
+            )
         }
-        return new Size(activeRect.width(), activeRect.height());
+        return Size(activeRect.width(), activeRect.height())
+    }
+
+    companion object {
+        private val TAG: String = Camera2MeteringTransform::class.java.simpleName
+        private val LOG: CameraLogger = CameraLogger.create(TAG)
     }
 }
